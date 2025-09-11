33b4e7794f4398609769231ca01ba71411016124
PawelGrzybek <desperatos@gmail.com>
2025-09-11 16:19:02 +0200

    Baza szkolna

--- BazaSzkolna/main.py
+++ BazaSzkolna/main.py
@@ -43,6 +43,8 @@ def sprawdz_czy_to_liczba(wartosc):
         except ValueError:
             print("To nie jest liczba")
             wartosc = input("Podaj liczbe \n")
+            #if wartosc =="":
+              #  return
     return wartosc
 
 
@@ -53,40 +55,47 @@ def dodaj():
 
 
 def dodaj_ucznia():
+    print("Dodaj ucznia")
     imie_i_nazwisko, klasa = dodaj()
     uczniowie.append(Uczen(imie_i_nazwisko, klasa))
 
 
 def dodaj_nauczyciela():
+    print("Dodaj nauczyciela")
     imie_i_nazwisko = input("Podaj imie i nazwisko ze spacja w srodku \n")
     przedmiot = input("Podaj przedmiot \n")
     i = 0
     klasy = []
     while 1:
-        i += 1
-        klasy[i] = input("Podaj klase w ktorej naucza pusta wartosc konczy wprowadzanie")
-        if klasy[i] == "":
+        klasa=input("Podaj klase w ktorej naucza pusta wartosc konczy wprowadzanie ")
+        klasy.append(klasa)
+        if klasa == "":
             break
         nauczyciele.append(Nauczyciel(imie_i_nazwisko, klasy, przedmiot))
 
 
 def dodaj_wychowawce():
+    print("Dodaj wychowawce")
     imie_i_nazwisko, klasa = dodaj()
     wychowawcy.append(Wychowawca(imie_i_nazwisko, klasa))
 
 
 def wyswietl_klasy():
     nazwa_klasy=input("Podaj nazwe klasy ")
-    print(f"Uczniowie klasy to{nazwa_klasy}")
+    print(f"Uczniowie klasy {nazwa_klasy} to")
     for uczen in uczniowie:
         if nazwa_klasy == uczen.klasa:
             #klasa.add(uczen.imie_i_nazwisko)
             print(uczen.imie_i_nazwisko)
+    else:
+        print("Nie znaleziono uczniow")
     print("Wychowawca to ")
     for wychowawca in wychowawcy:
         if wychowawca.klasa == nazwa_klasy:
             print(wychowawca.imie_i_nazwisko)
             break
+    else:
+        print("Nie znaleziono wychowawcy")
 
 
 def wyswietl_nauczycieli():
@@ -96,7 +105,9 @@ def wyswietl_nauczycieli():
         if nauczyciel.imie_i_nazwisko == wybrany:
             for i in range(len(nauczyciel.klasa)):
                 print(nauczyciel.klasa[i])
-
+        break
+    else:
+        print("Nie znaleziono nauczyciela")
 
 
 def wyswietl_wychowawce():
@@ -109,6 +120,8 @@ def wyswietl_wychowawce():
                 if wychowawca.klasa == uczniowie[i].klasa:
                     print(uczniowie[i].imie_i_nazwisko)
             break
+        else:
+            print("Nie znaleziono wychowawcy")
 
 
 
@@ -129,8 +142,6 @@ def wyswietl_uczniow():
                     print(nauczyciel.przedmiot)
         else:
             print("nie znaleziono ucznia")
-
-
 
 
 def utworz():
@@ -159,7 +170,7 @@ def zarzadzaj():
         print("2 Nauczyciel")
         print("3.Wychowawca")
         print("4.Klasa")
-        print("4.Koniec")
+        print("5.Koniec")
         opcja_zarzadzaj = input("Wybierz opcje używając numerow 1-8. 8 Konczy program \n")
         opcja_zarzadzaj = sprawdz_czy_to_liczba(opcja_zarzadzaj)
         if opcja_zarzadzaj == 1:
